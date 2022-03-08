02e89ca0fd0eb507c8351b7db74e88743f48d7b7
xndrbrgs <aborgescolon@gmail.com>
2022-03-07 21:29:32 -0500

    all mutations added for resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -44,8 +44,32 @@ const resolvers = {
 
         saveBook: async (parent, { newBook }, context) => {
             if (context.user) {
-                const updatedUser = await User.
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: newBook }},
+                    { new: true },
+                );
+
+                return updatedUser;
             }
-        }
-    }
-}
+
+            throw new AuthenticationError('User needs to be logged in')
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: {bookId} }},
+                    { new: true },
+                );
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('User needs to be logged in')
+        },
+    },
+};
+
+module.exports = resolvers;
