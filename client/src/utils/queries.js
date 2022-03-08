dc0e44dce78d9b00de8d933452709af9f71b1cd4
xndrbrgs <aborgescolon@gmail.com>
2022-03-07 20:47:58 -0500

    mutations were done last commit, queries corrected

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,64 +1,12 @@
 import gql from "graphql-tag";
 
-export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-        email
-      }
-    }
-  }
-`;
-
-export const ADD_USER = gql`
-  mutation addUser($email: String!, $password: String!, $username: String!) {
-    login(email: $email, password: $password, username: $username) {
-      token
-      user {
-        _id
-        username
-        email
-        bookCount
-        savedBooks {
-          authors
-          image
-          bookId
-          title
-          link
-          description
-        }
-      }
-    }
-  }
-`;
-
-export const SAVE_BOOK = gql`
-  mutation saveBook($bookInput: BookInput!) {
-    saveBook(bookInput: $bookInput) {
+export const GET_ME = gql`
+  {
+    me {
       _id
       username
       email
       bookCount
-      savedBooks {
-        authors
-        image
-        bookId
-        title
-        link
-        description
-      }
-    }
-  }
-`;
-
-export const REMOVE_BOOK = gql`
-  mutation removeBook($bookID: ID!) {
-    removeBook(bookID: $bookID) {
-      _id
-      username
       savedBooks {
         authors
         image
